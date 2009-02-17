c4fc897dfafbf37a172700fb76b3d00a433c41f9
Jesse Glick <jglick@java.net>
2009-02-17 23:15:07 +0000

    Indexed annotations can and should specify RetentionPolicy.SOURCE.

--- demo/api/src/main/java/demo/api/MenuItem.java
+++ demo/api/src/main/java/demo/api/MenuItem.java
@@ -1,9 +1,12 @@
 package demo.api;
 import java.awt.event.ActionListener;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import net.java.sezpoz.Indexable;
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.SOURCE)
 @Indexable(type=ActionListener.class)
 public @interface MenuItem {
     String menu();
