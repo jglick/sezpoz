3eab9847c525bf766d01c50dab6eabb4e1221dac
Jesse Glick <jglick@java.net>
2010-03-19 00:10:29 +0000

    Forgotten meta-annotations.

--- perftest/src/main/java/using_sezpoz/DemoService.java
+++ perftest/src/main/java/using_sezpoz/DemoService.java
@@ -1,6 +1,12 @@
 package using_sezpoz;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import net.java.sezpoz.Indexable;
 @Indexable(type=Runnable.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface DemoService {
     int type();
 }
